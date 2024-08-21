dcb3a18198518f2d0a6d6f4a7b8b5c0e9d1da606
zztkm <zztkm@tsurutatakumi.info>
2024-08-21 18:07:02 +0900

    update

--- install.bat
+++ install.bat
@@ -13,3 +13,6 @@ mklink /D %SRC% %DIST%
 set NYAGOS_CONFIG_DIST=%PWD%\windows\nyagos.lua
 set NYAGOS_CONFIG_SRC=%USERPROFILE%\.nyagos
 mklink %NYAGOS_CONFIG_SRC% %NYAGOS_CONFIG_DIST%
+
+mklink %USERPROFILE%\.ideavimrc %PWD%\ideavim\.ideavimrc
+
