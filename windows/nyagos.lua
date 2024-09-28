a95d61f7c03de43f743cba33016063f0d288799a
zztkm <zztkm@tsurutatakumi.info>
2024-09-28 23:28:29 +0900

    git branch 名を表示するようにした

--- windows/nyagos.lua
+++ windows/nyagos.lua
@@ -22,3 +22,23 @@ nyagos.alias["cp"] = "copy"
 nyagos.alias["gs"] = "git status"
 nyagos.alias["nv"] = "nvim"
 
+
+-- NYAGOS プロンプトに git のブランチ名を表示する
+-- refs: <https://outofmem.hatenablog.com/entry/2016/01/27/014352>
+nyagos.env.prompt='$L'.. nyagos.getenv('COMPUTERNAME') .. ':$P$G'
+share.org_prompter=nyagos.prompt
+
+nyagos.prompt = function(this)
+    local prompt_message = this
+    -- プロンプトにgit_branch_nameを表示する
+    local git_branch_name = nyagos.eval('git rev-parse --abbrev-ref HEAD 2>nul')
+    if (git_branch_name ~= '') then
+        -- もちろんgit_branch_nameに適当な色をつけたりしてもOK
+        prompt_message = prompt_message .. ' [' .. git_branch_name .. ']'
+    end
+
+    -- ここで改行をくっつける
+    prompt_message = prompt_message .. '$_$$$s'
+
+    return share.org_prompter('$e[36;40;1m'..prompt_message..'$e[37;1m')
+end
