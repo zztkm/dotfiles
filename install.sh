ce66641422c4e5f0599672a1c399f275b89e679e
zztkm <zztkm@tsurutatakumi.info>
2024-12-05 12:23:56 +0900

    update

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ ln -snf "${PWD}/nvim-config" "$HOME/.config/nvim"
 # .wezterm.lua を $HOME にリンク
 ln -snf "${PWD}/.config/.wezterm.lua" "$HOME/.wezterm.lua"
 
+ln -snf "${PWD}/ideavim/.ideavimrc" "$HOME/.ideavimrc"
+
