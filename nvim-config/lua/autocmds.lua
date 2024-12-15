7b815b850065a3446e4271664559a30c03ce0ab5
zztkm <zztkm@tsurutatakumi.info>
2024-12-15 14:31:56 +0900

    go fmt on save を無効化

--- nvim-config/lua/autocmds.lua
+++ nvim-config/lua/autocmds.lua
@@ -1,11 +1,11 @@
 -- go.nvim format on save
 -- https://github.com/ray-x/go.nvim?tab=readme-ov-file#format-on-save
-local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
-
+--local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--  pattern = "*.go",
+--  callback = function()
+--   require('go.format').goimports()
+--  end,
+--  group = format_sync_grp,
+--})
+--
