973f11df92bd68c31340a857448590980490f707
zztkm <zztkm@tsurutatakumi.info>
2024-12-28 14:02:42 +0900

    Ghostty の config を dotfiles 管理に変更する

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -e
 PWD=`pwd`
 
 ln -snf "${PWD}/nvim-config" "$HOME/.config/nvim"
+ln -snf "${PWD}/ghostty" "$HOME/.config/ghostty"
 # .wezterm.lua を $HOME にリンク
 ln -snf "${PWD}/.config/.wezterm.lua" "$HOME/.wezterm.lua"
 
