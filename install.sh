3388ad0a9b3979ed2eb801f08b10425fa0d85295
zztkm <33755694+zztkm@users.noreply.github.com>
2025-12-12 14:59:48 +0900

    tmux 設定を追加する

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ ln -snf "${PWD}/nvim-config" "$HOME/.config/nvim"
 ln -snf "${PWD}/ghostty" "$HOME/.config/ghostty"
 # .wezterm.lua を $HOME にリンク
 ln -snf "${PWD}/.config/.wezterm.lua" "$HOME/.wezterm.lua"
-
 ln -snf "${PWD}/ideavim/.ideavimrc" "$HOME/.ideavimrc"
+ln -snf "${PWD}/.tmux.conf" "$HOME/.tmux.conf"
 
