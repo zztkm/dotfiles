61fb5e3f75d4864ba183c18cd336de29fc94bcc1
zztkm <33755694+zztkm@users.noreply.github.com>
2026-03-31 15:24:38 +0900

    lazy に移行する

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -1,51 +1,19 @@
 -- Neovim 設定のエントリポイント
 
--- minpac のブートストラップ
-local function ensure_minpac()
-  local minpac_path = vim.fn.stdpath('data')..'/site/pack/minpac/opt/minpac'
-  if vim.fn.empty(vim.fn.glob(minpac_path)) > 0 then
-    vim.fn.system({'git', 'clone', 'https://github.com/k-takata/minpac.git', minpac_path})
-    vim.cmd [[packadd minpac]]
-  else
-    vim.cmd [[packadd minpac]]
-  end
-end
-
--- minpac を確実にロード
-ensure_minpac()
-
--- minpac の初期化とプラグイン設定
-vim.cmd [[
-  function! PackInit() abort
-    packadd minpac
-
-    call minpac#init()
-    call minpac#add('k-takata/minpac', {'type': 'opt'})
-
-    " プラグイン追加
-    call minpac#add('junegunn/fzf')
-    call minpac#add('junegunn/fzf.vim')
-    call minpac#add('neovim/nvim-lspconfig')
-    call minpac#add('williamboman/mason.nvim')
-    call minpac#add('williamboman/mason-lspconfig.nvim')
-    call minpac#add('bronson/vim-trailing-whitespace')
-    call minpac#add('hrsh7th/nvim-cmp')
-    call minpac#add('hrsh7th/cmp-nvim-lsp')
-    call minpac#add('hrsh7th/cmp-buffer')
-    call minpac#add('hrsh7th/cmp-emoji')
-	call minpac#add('L3MON4D3/LuaSnip')
-  endfunction
-
-  " 独自コマンドを定義
-  command! PackUpdate call PackInit() | call minpac#update()
-  command! PackClean  call PackInit() | call minpac#clean()
-  command! PackStatus packadd minpac | call minpac#status()
-]]
-
--- module を利用するには lua ディレクトリに lua/hoge.lua のようにファイルを作成して
--- require("hoge") のように読み込みます
-
 require("options")
-require("plugins/config")
 require("keymaps")
+
+-- lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
 
